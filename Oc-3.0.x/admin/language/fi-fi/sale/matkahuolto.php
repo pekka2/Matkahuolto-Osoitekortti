8f50f757e5c53677ab170545b2687f29add687b6
Pekka Mansikka <pekka2@users.noreply.github.com>
2023-09-13 16:50:28 +0300

    Create matkahuolto.php

---
+++ Oc-3.0.x/admin/language/fi-fi/sale/matkahuolto.php
@@ -0,0 +1,97 @@
+<?php
+// Heading
+$_['heading_title']       = 'Matkahuollon osoitekortti';
+
+// Text
+$_['text_extension']      = 'Laajennukset';
+$_['text_success']        = 'Olet muokannut Matkahuolto toimitustapaa onnistuneesti!';
+$_['text_edit']           = 'Luo Matkahuollon osoitekortti';
+$_['text_description']    = 'Tämän lomakkeen avulla voit hakea Matkahuollon osoitekortin.  Lomakkeen tiedot on suurelta osin esitäytety tietokannassa olevien tilaukseen liittyvien tietojen pohjalta. Tarkista ja korjaa esitäytetyt tiedot. Esimerkiksi pakettien lukumäärä on oletuksena yksi. Samoin lähetyksen painossa ja tilavuudessa ei ole otettu huomioon pakkaustarvikkeiden vaatimaa lisäpainoa- ja tilavuutta. Mikäli lomake vaikuttaa sinusta hyvin suurelta, niin huomaa, että ainoastaan tähdellä nerkityt kentät ovat pakollisia, mikäli sinulla ei ole tarvetta käyttää muita lomakkeella olevia toimintoja.';
+$_['text_order']           = 'Tilaukset';
+
+// Entry
+$_['entry_cod_sum']                 = 'Bussiennakon summa';
+$_['entry_cod_currency']            = 'Bussiennakon valuutta';
+$_['entry_cod_iban']                = 'Bussiennakon pankkitili (IBAN)';
+$_['entry_cod_bic']                 = 'Bussiennakon pankkitunnus (BIC)';
+$_['entry_cod_reference']           = 'Bussiennakon pankkiviite';
+$_['entry_delivery']                = 'Jaettava lähetys';
+$_['entry_delivery_payer']          = 'Jaon maksaja';
+$_['entry_delivery_remark']         = 'Jakohuomautus';
+$_['entry_departure_place_code']    = 'Lähtöpaikan/-aseman tunnus';
+$_['entry_departure_place_name']    = 'Lähtöpaikan/-aseman nimi';
+$_['entry_destination_place_code']  = 'Määräpaikan/-aseman tunnus';
+$_['entry_destination_place_name']  = 'Määräpaikan/-aseman nimi';
+$_['entry_doc_type']                = 'Palautettavan kuljetusdokum. tyyppi';
+$_['entry_goods']                   = 'Lähetyksen sisältö';
+$_['entry_handover']                = 'Erikoisluovutus, esim. turvaluovutus tms.';
+$_['entry_handover_remark_1']       = 'Luovutushuomautus1';
+$_['entry_handover_remark_2']       = 'Luovutushuomautus2';
+$_['entry_handover_remark_3']       = 'Luovutushuomautus3';
+$_['entry_message_type']            = 'Sanoman tyyppi';
+$_['entry_package_id']              = 'Paketin tunnus';
+$_['entry_packages']                = 'Lähetyksen pakettimäärä ';
+$_['entry_password']                = 'Salasana';
+$_['entry_payer_address']           = 'Maksajan katuosoite';
+$_['entry_payer_city']              = 'Maksajan paikkakunta';
+$_['entry_payer_code']              = 'Rahdin maksajan koodi';
+$_['entry_payer_contact_name']      = 'Maksajan yhteyshenkilö';
+$_['entry_payer_contact_number']    = 'Maksajan yhteysnumero';
+$_['entry_payer_country']           = 'Maksajan maa';
+$_['entry_payer_email']             = 'Maksajan sähköpostiosoite';
+$_['entry_payer_id']                = 'Maksajan asiakasnumero';
+$_['entry_payer_name']              = 'Maksajan nimi ';
+$_['entry_payer_postal']            = 'Maksajan postinumero';
+$_['entry_payer_reference']         = 'Maksajan viite';
+$_['entry_pickup']                  = 'Noudettava lähetys';
+$_['entry_pickup_payer']            = 'Noudon maksaja';
+$_['entry_pickup_remark']           = 'Noutohuomautus';
+$_['entry_products']                = 'Matkahuollon tuotekoodi';
+$_['entry_product_name']            = 'Matkahuollon tuotteen nimi ';
+$_['entry_receiver_address']        = 'Vastaanottajan katuosoite';
+$_['entry_receiver_city']           = 'Vastaanottajan paikkakunta';
+$_['entry_receiver_contact_name']   = 'Vastaanottajan yhteyshenkilö';
+$_['entry_receiver_contact_number'] = 'Vastaanottajan yhteysnumero';
+$_['entry_receiver_country']        = 'Vastaanottajan maa';
+$_['entry_receiver_email']          = 'Vastaanottajan sähköpostiosoite ';
+$_['entry_receiver_id']             = 'Vastaanottajan asiakasnumero';
+$_['entry_receiver_name']           = 'Vastaanottajan nimi 1';
+$_['entry_receiver_name2']          = 'Vastaanottajan nimi 2';
+$_['entry_receiver_postal']         = 'Vastaanottajan postinumero';
+$_['entry_receiver_reference']      = 'Vastaanottajan viite';
+$_['entry_remarks']                 = 'Huomautukset';
+$_['entry_sender_address']          = 'Lähettäjän katuosoite';
+$_['entry_sender_city']             = 'Lähettäjän paikkakunta';
+$_['entry_sender_contact_name']     = 'Lähettäjän yhteyshenkilö';
+$_['entry_sender_contact_number']   = 'Lähettäjän yhteysnumero';
+$_['entry_sender_country']          = 'Lähettäjän maa';
+$_['entry_sender_email']            = 'Lähettäjän sähköpostiosoite';
+$_['entry_sender_id']               = 'Lähettäjän Id';
+$_['entry_sender_name']             = 'Lähettäjän nimi';
+$_['entry_sender_name_2']             = 'Lähettäjän nimi 2';
+$_['entry_sender_postal']           = 'Lähettäjän postinumero';
+$_['entry_sender_reference']        = 'Lähettäjän viite';
+$_['entry_shipment_type']           = 'Lähetyksen tyyppi';
+$_['entry_special_handling']        = 'Erikoiskäsittelymerkintä';
+$_['entry_user_id']                 = 'Käyttäjän ID';
+$_['entry_vak_code']                = 'Vaarallisen aineen koodi';
+$_['entry_vak_description']         = 'Vaarallisen aineen kuvaus';
+$_['entry_version']                 = 'Sanomakuvauksen versionumero';
+$_['entry_volume']                  = 'Lähetyksen kokonaistilavuus';
+$_['entry_weight']                  = 'Lähetyksen kokonaispaino';
+
+// help
+$_['help_matkahuolto']              = '';
+
+// Error
+$_['error_matkahuolto_account_user_id']    = 'Käyttäjätunnus on pakollinen!';
+$_['error_matkahuolto_account_sender_id']  = 'Lähettäjän asiakasnumeo  on pakollinen!';
+$_['error_matkahuolto_account_password']   = 'Salasana on pakollinen!';
+$_['error_shipment_type']                  = 'Lähetystyyppi on pakollinen!';
+$_['error_message_type']                   = 'Viestin tyyppi on pakollinen!';
+$_['error_packages']                       = 'Pakettien määrä on pakollinen!';
+$_['error_receiver_name']                  = 'Vastaanottajan nimi on pakollinen!';
+$_['error_receiver_postal']                = 'Vastaanottajan postinumero on pakollinen!';
+$_['error_receiver_city']                  = 'Vastaanottajan paikkakunta on pakollinen!';
+$_['error_product_code']                   = 'Matkahuollon tuotekoodi on pakollinen!';
+$_['error_permission']                     = 'Warning: You do not have permission to modify shipping Matkahuolto!';
